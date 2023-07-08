d4c6c672a328c6734cd0e0808db529611782b59a
louis.lebrun <louis.lebrun@trusthq>
2023-07-08 11:48:52 +0200

    Testing Astro

---
+++ src/components/Card.jsx
@@ -0,0 +1,7 @@
+export default function Card(props) {
+    return (
+        <div className="rounded-lg p-4 border-t-8 border-l-8 border-cyan-500">
+            <p>hey</p>
+        </div>
+    )
+}
---
+++ src/components/Portrait.jsx
@@ -0,0 +1,13 @@
+export default function Portrait(props) {
+    return (
+        <div className="flex items-center">
+            <img
+                className="rounded-full w-1/2"
+                src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.rainforestanimals.net%2Fwp-content%2Fuploads%2F2018%2F12%2Fredeyedtreefrogsmall.jpg&f=1&nofb=1&ipt=ca7a614abb5a3064527cd3ca04b2ae755df23049fd1c5e338f1a3df7c09bd275&ipo=images" />
+            <div className="w-10"></div>
+            <p className="text-center font-bold text-4xl">
+                Hi, my name is Louis
+            </p>
+        </div >
+    )
+}
